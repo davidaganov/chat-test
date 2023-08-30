493e71c69e603365130fd5e4afbc98d2db90568d
David <davidaganov21@gmail.com>
2023-08-30 14:31:58 +0500

    test

--- src/Carrot.vue
+++ src/Carrot.vue
@@ -1,34 +1,50 @@
 <template>
-  <form>
-    <input
-      type="text"
-      v-model="form.id"
-    />
-    <input
-      type="text"
-      v-model="form.name"
-    />
-    <input
-      type="text"
-      v-model="form.email"
-    />
-    <button
-      @click="handleAuth"
-      type="button"
+  <div class="container">
+    <form
+      class="form"
+      v-if="!auth"
     >
-      Submit
-    </button>
-  </form>
+      <input
+        class="form__input"
+        type="text"
+        name="name"
+        placeholder="Введите имя"
+        v-model="form.name"
+      />
+      <input
+        class="form__input"
+        type="text"
+        name="email"
+        placeholder="Введите email"
+        v-model="form.email"
+      />
+      <button
+        type="button"
+        class="form__button"
+        @click="handleAuth"
+      >
+        Submit
+      </button>
+    </form>
+
+    <div
+      class="user"
+      v-else
+    >
+      <p class="user__name">{{ form.name }}</p>
+      <p class="user__email">{{ form.email }}</p>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { onMounted, reactive } from "vue"
+import { onMounted, ref, reactive } from "vue"
 import CryptoJS from "crypto-js"
 
+const auth = ref(false)
 const form = reactive({
-  id: "",
-  name: "Test",
-  email: "purelyugly@gmail.com"
+  name: "",
+  email: ""
 })
 
 const handleAuth = () => {
@@ -36,21 +52,98 @@ const handleAuth = () => {
 }
 
 const authCarrot = () => {
-  const userId = form.id
+  const date = new Date()
+  const RegDate = date.toISOString()
+
+  const userId = form.name
   const userAuthKey = import.meta.env.VITE_USER_AUTH_KEY
 
   const hash = CryptoJS.HmacSHA256(userId, userAuthKey)
   carrotquest.auth(userId, hash)
 
-  // carrotquest.identify([
-  //   {
-  //     name: form.name,
-  //     email: form.email
-  //   }
-  // ])
+  carrotquest.identify({
+    $name: form.name,
+    $email: form.email,
+    RegDate
+  })
+
+  auth.value = true
 }
 
-// onMounted(() => {
-//   authCarrot()
-// })
+onMounted(() => {
+  localStorage.removeItem("carrotquest_last_activity")
+  localStorage.removeItem("carrotquest_requests")
+  localStorage.removeItem("carrotquest_data")
+
+  const cookies = document.cookie.split(";")
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[i]
+    const eqPos = cookie.indexOf("=")
+    const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie
+    document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;"
+    document.cookie = name + "=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;"
+  }
+})
 </script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+
+.container {
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  width: 100%;
+}
+
+.form {
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  max-width: 400px;
+  width: 100%;
+}
+
+.form__input {
+  padding: 8px 10px;
+  font-family: "Roboto", sans-serif;
+  font-size: 18px;
+  color: #070707;
+  border: 1px solid #b1b1b1;
+  border-radius: 10px;
+}
+
+.form__button {
+  padding: 8px 10px;
+  background-color: #004a53;
+  border-radius: 10px;
+  border: none;
+  color: white;
+  font-family: "Roboto", sans-serif;
+  font-size: 18px;
+  cursor: pointer;
+}
+
+.user {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.user__name {
+  padding: 0;
+  font-family: "Roboto", sans-serif;
+  font-size: 24px;
+  color: #004a53;
+}
+
+.user__email {
+  padding: 0;
+  font-family: "Roboto", sans-serif;
+  font-size: 24px;
+  color: #004a53;
+}
+</style>
